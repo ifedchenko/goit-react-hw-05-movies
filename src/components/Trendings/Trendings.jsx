271e851c262c800d5d97277ccb24ed7756207f72
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-30 19:01:58 +0300

    add images

--- src/components/Trendings/Trendings.jsx
+++ src/components/Trendings/Trendings.jsx
@@ -1,31 +1,41 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 import fetchTrendings from 'services/fetchTrendings';
 import MovieList from 'components/MovieList/MovieList';
 import css from './Trendings.module.css';
 
 const Trendings = () => {
   const [movies, setMovies] = useState([]);
-  const [isLoaderShown, setIsLoaderShown] = useState(false);
+  const [isLoaderShown, setIsLoaderShown] = useState(true);
 
   useEffect(() => {
     async function fetchTrendMovies() {
       try {
-        setIsLoaderShown(true);
         const result = await fetchTrendings();
         setMovies(result.results);
         setIsLoaderShown(false);
       } catch (error) {
+        setIsLoaderShown(false);
         console.log(error);
       }
     }
+
     fetchTrendMovies();
   }, []);
 
   return (
     <div className={css.trendings}>
       <h1>Trendings today</h1>
-      {isLoaderShown && 'Loading ...'}
-      <MovieList movies={movies} />
+      {isLoaderShown && (
+        <div className={css.trendingsLoader}>
+          <CircularProgress />
+        </div>
+      )}
+      {!isLoaderShown && (
+        <div>
+          <MovieList movies={movies} />
+        </div>
+      )}
     </div>
   );
 };
