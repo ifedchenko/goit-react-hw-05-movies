acfdcafcc6ae5e2d3abe6aef8b368d24215adcfd
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-09-03 20:10:39 +0300

    add Material UI appbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,32 +1,37 @@
 import { NavLink } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+
 import css from './Navigation.module.css';
 
-let activeClassName = {
-  color: 'rgb(255, 255, 255)',
-  backgroundColor: 'rgb(12, 46, 86)',
-};
+// let activeClassName = {
+//   // color: 'rgb(255, 255, 255)',
+//   // backgroundColor: 'rgb(12, 46, 86)',
+// };
 
 const Navigation = () => (
-  <header className={css.header}>
-    <div className={css.container}>
-      <nav className={css.navigation}>
-        <NavLink
-          to="/"
-          className={css.link}
-          style={({ isActive }) => (isActive ? activeClassName : undefined)}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/movies"
-          className={css.link}
-          style={({ isActive }) => (isActive ? activeClassName : undefined)}
-        >
-          Movies
-        </NavLink>
-      </nav>
-    </div>
-  </header>
+  <AppBar position="static">
+    <header className={css.header}>
+      <div className={css.container}>
+        <nav className={css.navigation}>
+          <NavLink
+            to="/"
+            className={css.link}
+            // style={({ isActive }) => (isActive ? activeClassName : undefined)}
+          >
+            <Button variant="contained">Home</Button>
+          </NavLink>
+          <NavLink
+            to="/movies"
+            className={css.link}
+            // style={({ isActive }) => (isActive ? activeClassName : undefined)}
+          >
+            <Button variant="contained">Movies</Button>
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  </AppBar>
 );
 
 export default Navigation;
